3d5857f86e6d0bfb1ede9b604e9e4b953931cfa6
MannaRASSAA <ncib.imen@gmail.com>
2016-04-06 17:11:21 +0200

    Mise à jour des templates et de la classe Bien

--- OeUp.Project.Web/wwwroot/app/search/result/resultController.js
+++ OeUp.Project.Web/wwwroot/app/search/result/resultController.js
@@ -3,6 +3,7 @@ var oeup;
     'use strict';
     var ResultController = (function () {
         function ResultController($scope, $location, $log, uiGmapGoogleMapApi) {
+            var _this = this;
             var vm = this;
             $log.info("ResultController called");
             uiGmapGoogleMapApi.then(function (maps) {
@@ -12,6 +13,7 @@ var oeup;
                 if (typeof _.object === 'undefined') {
                     _.object = _.zipObject;
                 }
+                _this.map = { center: { latitude: 48.8965812, longitude: 2.318375999999944 }, zoom: 13, pointList: [], options: { streetViewControl: false } };
             });
         }
         return ResultController;
