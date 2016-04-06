9c873bcf961653b10a486a0db1371ace3fc91ab3
Firlej Jérôme <Firlej Jrme>
2016-04-06 20:14:27 +0200

    youhou

--- OeUp.Project.Web/wwwroot/app/search/result/resultController.js
+++ OeUp.Project.Web/wwwroot/app/search/result/resultController.js
@@ -4,8 +4,35 @@ var oeup;
     var ResultController = (function () {
         function ResultController($scope, $location, $log, uiGmapGoogleMapApi) {
             'ngInject';
+            this.HighlightObject = function (point) {
+                point.class = "black";
+            };
+            this.UnHighlightObject = function (point) {
+                point.class = "white";
+            };
             var vm = this;
             $log.info("ResultController called");
+            vm.objects = [{
+                    img: "http://www.lettre-gratuite.fr/files/2013/03/place-parking.jpg",
+                    texte: "lololol",
+                    price: 10,
+                    id: 1,
+                    position: {
+                        latitude: 48.8965812, longitude: 2.318375999999944
+                    },
+                    class: "white"
+                },
+                {
+                    img: "http://img0.gtsstatic.com/faits-divers/mal-foutue-cette-place-de-parking_646_w620.jpg",
+                    texte: "lorem ipsum",
+                    price: 10,
+                    id: 2,
+                    position: {
+                        latitude: 48.8971468, longitude: 2.1845104
+                    },
+                    class: "white"
+                }
+            ];
             uiGmapGoogleMapApi.then(function (maps) {
                 if (typeof _.contains === 'undefined') {
                     _.contains = _.includes;
@@ -14,28 +41,25 @@ var oeup;
                     _.object = _.zipObject;
                 }
                 maps.visualRefresh = true;
-                var boueb = {
-                    id: 1,
-                    position: {
-                        latitude: 48.8965812, longitude: 2.318375999999944
-                    },
-                    click: function () {
+                vm.objects.forEach(function (point) {
+                    point.click = function () {
                         $location.url('/rechercher');
-                    }
-                };
-                vm.map.pointList.push(boueb);
-                var boueb2 = {
-                    id: 2,
-                    position: {
-                        latitude: 48.8971468, longitude: 2.1845104
-                    },
-                    click: function () {
-                        $location.url('/rechercher');
-                    }
-                };
-                vm.map.pointList.push(boueb2);
+                    };
+                    point.mouseover = function () {
+                        vm.HighlightObject(point);
+                    };
+                    vm.map.pointList.push(point);
+                });
             });
             vm.map = { center: { latitude: 48.8965812, longitude: 2.318375999999944 }, zoom: 13, pointList: [], options: { streetViewControl: false } };
+            vm.map.markers2Events = {
+                mouseover: function (marker, eventName, model, args) {
+                    vm.HighlightObject(model);
+                },
+                mouseout: function (marker, eventName, model, args) {
+                    vm.UnHighlightObject(model);
+                }
+            };
         }
         return ResultController;
     }());
