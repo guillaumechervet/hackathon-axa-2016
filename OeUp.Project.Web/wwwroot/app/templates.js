53800845c711bdb11bd80c6f0f0cdd3113b4685a
CHERVET Guillaume <guillaume.chervet@gmail.com>
2016-04-06 19:50:39 +0200

    Mise à jour

--- OeUp.Project.Web/wwwroot/app/templates.js
+++ OeUp.Project.Web/wwwroot/app/templates.js
@@ -17,45 +17,41 @@
 
 
   $templateCache.put('/wwwroot/app/menu/menu.html',
-    "<img src=\"/images/logo.png\" alt=\"Logo\" class=\"img-responsive\"><nav class=\"navbar navbar-default\" ng-controller=\"MenuController as menu\"><div class=\"container-fluid\"><!-- Brand and toggle get grouped for better mobile display --><div class=\"navbar-header\"><button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\"><span class=\"sr-only\">Toggle navigation</span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span></button> <a class=\"navbar-brand\" href=\"/\">Blockchain</a></div><!-- Collect the nav links, forms, and other content for toggling --><div class=\"collapse navbar-collapse\" ng-if=\"!menu.isHome()\"><ul class=\"nav navbar-nav\"><!--\r" +
+    "<img src=\"/images/logo.png\" alt=\"Logo\" class=\"img-responsive\"><!--<nav class=\"navbar navbar-default\" ng-controller=\"MenuController as menu\" >\r" +
     "\n" +
-    "        <li class=\"active\"><a href=\"#\">Link <span class=\"sr-only\">(current)</span></a></li>\r" +
+    "  <div class=\"container-fluid\">\r" +
     "\n" +
-    "       \r" +
+    "    <div class=\"navbar-header\">\r" +
     "\n" +
-    "        <li><a href=\"#\">Projets</a></li> --><li ng-class=\"menu.isActive('/rechercher') ? 'active' : ''\"><a href=\"/rechercher\">Exemple menu</a></li></ul><!--\r" +
+    "      <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\">\r" +
     "\n" +
-    "      <ul class=\"nav navbar-nav navbar-right\">\r" +
+    "        <span class=\"sr-only\">Toggle navigation</span>\r" +
     "\n" +
-    "        <li><a href=\"#\">Link</a></li>\r" +
+    "        <span class=\"icon-bar\"></span>\r" +
     "\n" +
-    "        <li class=\"dropdown\">\r" +
+    "        <span class=\"icon-bar\"></span>\r" +
     "\n" +
-    "          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>\r" +
+    "        <span class=\"icon-bar\"></span>\r" +
     "\n" +
-    "          <ul class=\"dropdown-menu\">\r" +
+    "      </button>\r" +
     "\n" +
-    "            <li><a href=\"#\">Action</a></li>\r" +
-    "\n" +
-    "            <li><a href=\"#\">Another action</a></li>\r" +
-    "\n" +
-    "            <li><a href=\"#\">Something else here</a></li>\r" +
-    "\n" +
-    "            <li role=\"separator\" class=\"divider\"></li>\r" +
-    "\n" +
-    "            <li><a href=\"#\">Separated link</a></li>\r" +
-    "\n" +
-    "          </ul>\r" +
-    "\n" +
-    "        </li>\r" +
-    "\n" +
-    "      </ul>\r" +
+    "      <a class=\"navbar-brand\" href=\"/\">Blockchain</a>\r" +
     "\n" +
     "    </div>\r" +
     "\n" +
-    "  </div>\r" +
+    "\r" +
     "\n" +
-    "  --><!-- /.container-fluid --></div></div></nav>"
+    "    <div class=\"collapse navbar-collapse\" ng-if=\"!menu.isHome()\">\r" +
+    "\n" +
+    "      <ul class=\"nav navbar-nav\">\r" +
+    "\n" +
+    "         <li ng-class=\"menu.isActive('/rechercher') ? 'active' : ''\"><a href=\"/rechercher\" >Exemple menu</a></li>\r" +
+    "\n" +
+    "      </ul>\r" +
+    "\n" +
+    "      \r" +
+    "\n" +
+    "</nav>-->"
   );
 
 
@@ -68,7 +64,7 @@
     "\n" +
     "    <li class=\"active\">Ajout bien</li>\r" +
     "\n" +
-    "    </ol></script><div ng-include=\"'breadcrumbAddBien.html'\"></div><div style=\"row\"><h1>Ajout bien</h1><div><form name=\"form\" role=\"form\" class=\"form-horizontal\" enctype=\"multipart/form-data\" novalidate mw-submit=\"vm.submit(form)\"><h2>Formulaire</h2><div class=\"form-group\" mw-container=\"form.uType\"><label for=\"uType\" class=\"col-sm-3 control-label\">Quoi*:</label><div class=\"col-sm-4\"><select id=\"uType\" name=\"uType\" class=\"form-control\" ng-model=\"vm.Model.Type\" ng-options=\"c as c for c in vm.Types\" mw-validate=\"vm.Rules.Types\"><option value=\"\">- Sélectionner -</option></select><span mw-error=\"form.uType\"></span></div></div><div class=\"form-group\" mw-container=\"form.uTitle\"><label class=\"col-sm-3 control-label\">Ou* :</label><div class=\"col-sm-4\"><input type=\"text\" name=\"uTitle\" ng-model=\"vm.Model.Title\" class=\"form-control\" mw-validate=\"vm.Rules.Title\"> <span mw-error=\"form.uTitle\"></span></div></div><div class=\"form-group\" mw-container=\"form.uTitle\"><label class=\"col-sm-3 control-label\">Titre* :</label><div class=\"col-sm-4\"><input type=\"text\" name=\"uTitle\" ng-model=\"vm.Model.Title\" class=\"form-control\" mw-validate=\"vm.Rules.Title\"> <span mw-error=\"form.uTitle\"></span></div></div><div class=\"form-group\" mw-container=\"form.uDateDebut\"><label class=\"col-sm-3 control-label\">Date début* :</label><div class=\"col-sm-4\"><input type=\"text\" name=\"uDateDebut\" ng-model=\"vm.Model.DateDebut\" class=\"form-control\" uib-datepicker-popup=\"dd/MM/yyyy\" mw-validate=\"vm.Rules.DateDebut\"> <span mw-error=\"form.uDateDebut\"></span></div></div><div class=\"form-group\" mw-container=\"form.uDateFin\"><label class=\"col-sm-3 control-label\">Date fin* :</label><div class=\"col-sm-4\"><input type=\"text\" name=\"uDateFin\" ng-model=\"vm.Model.DateFin\" class=\"form-control\" uib-datepicker-popup=\"dd/MM/yyyy\" mw-validate=\"vm.Rules.DateFin\"> <span mw-error=\"form.uDateFin\"></span></div></div><div class=\"form-group\" mw-container=\"form.uComment\"><label class=\"col-sm-3 control-label\">Description :</label><div class=\"col-sm-4\"><textarea ng-model=\"vm.Model.Comment\" class=\"form-control\" id=\"uComment\" name=\"uComment\" rows=\"4\" cols=\"30\" mw-validate=\"vm.Rules.Comment\"></textarea><span mw-error=\"form.uComment\"></span></div></div></form></div><div class=\"form-group\"><div class=\"col-sm-3\"></div><div class=\"col-sm-4\"><button type=\"submit\" class=\"btn btn-success\">Ajouter</button></div></div></div><div ng-include=\"'breadcrumbAddBien.html'\"></div>"
+    "    </ol></script><div ng-include=\"'breadcrumbAddBien.html'\"></div><div style=\"row\"><h1>Ajout bien</h1><div><form name=\"form\" role=\"form\" class=\"form-horizontal\" enctype=\"multipart/form-data\" novalidate mw-submit=\"vm.submit(form)\"><h2>Formulaire</h2><div class=\"form-group\" mw-container=\"form.uType\"><label for=\"uType\" class=\"col-sm-3 control-label\">Quoi*:</label><div class=\"col-sm-4\"><select id=\"uType\" name=\"uType\" class=\"form-control\" ng-model=\"vm.Model.Type\" ng-options=\"c as c for c in vm.Types\" mw-validate=\"vm.Rules.Types\"><option value=\"\">- Sélectionner -</option></select><span mw-error=\"form.uType\"></span></div></div><div class=\"form-group\" mw-container=\"form.uTitle\"><label class=\"col-sm-3 control-label\">Ou* :</label><div class=\"col-sm-4\"><input type=\"text\" name=\"uTitle\" ng-model=\"vm.Model.Title\" class=\"form-control\" mw-validate=\"vm.Rules.Title\"> <span mw-error=\"form.uTitle\"></span></div></div><div class=\"form-group\" mw-container=\"form.uTitle\"><label class=\"col-sm-3 control-label\">Titre* :</label><div class=\"col-sm-4\"><input type=\"text\" name=\"uTitle\" ng-model=\"vm.Model.Title\" class=\"form-control\" mw-validate=\"vm.Rules.Title\"> <span mw-error=\"form.uTitle\"></span></div></div><div class=\"form-group\" mw-container=\"form.uDateDebut\"><label class=\"col-sm-3 control-label\">Date début* :</label><div class=\"col-sm-4\"><input type=\"text\" name=\"uDateDebut\" ng-model=\"vm.Model.DateDebut\" class=\"form-control\" uib-datepicker-popup=\"dd/MM/yyyy\" mw-validate=\"vm.Rules.DateDebut\"> <span mw-error=\"form.uDateDebut\"></span></div></div><div class=\"form-group\" mw-container=\"form.uDateFin\"><label class=\"col-sm-3 control-label\">Date fin* :</label><div class=\"col-sm-4\"><input type=\"text\" name=\"uDateFin\" ng-model=\"vm.Model.DateFin\" class=\"form-control\" uib-datepicker-popup=\"dd/MM/yyyy\" mw-validate=\"vm.Rules.DateFin\"> <span mw-error=\"form.uDateFin\"></span></div></div><div class=\"form-group\" mw-container=\"form.uComment\"><label class=\"col-sm-3 control-label\">Description :</label><div class=\"col-sm-4\"><textarea ng-model=\"vm.Model.Comment\" class=\"form-control\" id=\"uComment\" name=\"uComment\" rows=\"4\" cols=\"30\" mw-validate=\"vm.Rules.Comment\"></textarea><span mw-error=\"form.uComment\"></span></div></div></form></div><div class=\"form-group\" style=\"height:60px\"><div class=\"col-sm-3\"></div><div class=\"col-sm-4\"><button type=\"submit\" class=\"btn btn-success\">Ajouter</button></div></div></div><div ng-include=\"'breadcrumbAddBien.html'\"></div>"
   );
 
 
@@ -79,12 +75,7 @@
     "\n" +
     "    <li class=\"active\">Proposer</li>\r" +
     "\n" +
-<<<<<<< HEAD
-    "    </ol></script><div ng-include=\"'breadcrumbProposer.html'\"></div><h1>Proposer vos biens</h1><div style=\"height:40px\"><button type=\"button\" class=\"btn btn-default pull-right\" ng-click=\"vm.navAdd()\">Ajouter un bien</button></div><div ng-if=\"vm.Biens.length >0\"><h2>Liste des biens</h2><table class=\"table\" ng-repeat=\"bien in vm.Biens\"><thead><tr><th>Titre</th><th>Date</th><th>Action</th></tr></thead><tbody><tr><td><span>Loué du {{bien.DateDebut | date:'dd/MM/yyyy' }} au {{bien.DateFin | date:'dd/MM/yyyy' }} : {{bien.Frequence}} de {{bien.DateDebut | date:'HH:mm' }} au {{bien.DateFin | date:'HH:mm' }}</span></td></tr><tr class=\"height-20\"><td><span ng-bind=\"bien.Titre\"></span></td><td><span>{{bien.DateDebut | date:'dd/MM/yyyy' }}</span> <span>{{bien.DateFin | date:'dd/MM/yyyy' }}</span></td><td><button ng-click=\"vm.remove(bien)\" type=\"button\" class=\"btn btn-danger\" uib-popover=\"Supprimer\" popover-trigger=\"mouseenter\"><span class=\"glyphicon glyphicon-remove\"></button></td></tr></tbody></table><div ng-if=\"vm.Biens.length <=0\" class=\"oeup-empty\"><p>Aucun bien déclaré.</p></div><div style=\"height:40px\"><button type=\"button\" class=\"btn btn-default pull-right\" ng-click=\"vm.navAdd()\">Ajouter un bien</button></div></div><div ng-include=\"'breadcrumbProposer.html'\"></div>"
-=======
-    "    </ol></script><div ng-include=\"'breadcrumbProposer.html'\"></div><h1>Proposer vos biens</h1><button type=\"button\" class=\"btn btn-default pull-right\" ng-bind=\"vm.navAdd()\">Ajouter un bien</button><div ng-if=\"vm.Biens.length >0\"><h2>Liste des biens</h2><div class=\"row\"><div class=\"col-sm-6 col-md-4\" ng-repeat=\"bien in vm.Biens\"><div class=\"thumbnail\"><img ng-src=\"{{bien.MainPhoto}}\"><div class=\"caption\"><h3>{{bien.Titre}}</h3><p><span>Loué du {{bien.DateDebut | date:'dd/MM/yyyy' }} au {{bien.DateFin | date:'dd/MM/yyyy' }} : {{bien.Frequence}} de {{bien.DateDebut | date:'HH:mm' }} au {{bien.DateFin | date:'HH:mm' }}</span> {{bien.Description}}</p><p><button ng-click=\"vm.remove(bien)\" type=\"button\" class=\"btn btn-danger\" uib-popover=\"Supprimer\" popover-trigger=\"mouseenter\"><span class=\"glyphicon glyphicon-remove\"></button> <button ng-click=\"vm.edit(bien)\" type=\"button\" class=\"btn btn-edit\" uib-popover=\"Editer\" popover-trigger=\"mouseenter\"><span class=\"glyphicon glyphicon-edit\"></button></p></div></div></div></div><button type=\"button\" class=\"btn btn-default pull-right\" ng-bind=\"vm.navAdd()\">Ajouter un bien</button></div><div ng-if=\"vm.Biens.length <=0\" class=\"oeup-empty\"><p>Aucun bien déclaré.</p></div><div ng-include=\"'breadcrumbProposer.html'\"></div>"
-
->>>>>>> d2ceb547194af3240b8b3312da5b4cfd99e17434
+    "    </ol></script><div ng-include=\"'breadcrumbProposer.html'\"></div><h1>Proposer vos biens</h1><div style=\"height:40px\"><button type=\"button\" class=\"btn btn-default pull-right\" ng-click=\"vm.navAdd()\">Ajouter un bien</button></div><div ng-if=\"vm.Biens.length >0\"><h2>Liste des biens</h2><div class=\"row\"><div class=\"col-sm-6 col-md-4\" ng-repeat=\"bien in vm.Biens\"><div class=\"thumbnail\"><img ng-src=\"{{bien.MainPhoto}}\"><div class=\"caption\"><h3>{{bien.Titre}}</h3><p><span>Loué du {{bien.DateDebut | date:'dd/MM/yyyy' }} au {{bien.DateFin | date:'dd/MM/yyyy' }} : {{bien.Frequence}} de {{bien.DateDebut | date:'HH:mm' }} au {{bien.DateFin | date:'HH:mm' }}</span> {{bien.Description}}</p><p><button ng-click=\"vm.remove(bien)\" type=\"button\" class=\"btn btn-danger\" uib-popover=\"Supprimer\" popover-trigger=\"mouseenter\"><span class=\"glyphicon glyphicon-remove\"></button> <button ng-click=\"vm.edit(bien)\" type=\"button\" class=\"btn btn-edit\" uib-popover=\"Editer\" popover-trigger=\"mouseenter\"><span class=\"glyphicon glyphicon-edit\"></button></p></div></div></div></div><button type=\"button\" class=\"btn btn-default pull-right\" ng-bind=\"vm.navAdd()\">Ajouter un bien</button></div><div ng-if=\"vm.Biens.length <=0\" class=\"oeup-empty\"><p>Aucun bien déclaré.</p></div><div style=\"height:40px\"><button type=\"button\" class=\"btn btn-default pull-right\" ng-click=\"vm.navAdd()\">Ajouter un bien</button></div><div ng-include=\"'breadcrumbProposer.html'\"></div>"
   );
 
 
@@ -95,7 +86,7 @@
     "\n" +
     "    <li class=\"active\">Recherche</li>\r" +
     "\n" +
-    "    </ol></script><div style=\"row\"><div ng-include=\"'breadcrumbAddBien.html'\"></div><h1>Rechercher</h1><div><form name=\"form\" role=\"form\" class=\"form-horizontal\" enctype=\"multipart/form-data\" novalidate mw-submit=\"vm.submit(form)\"><div class=\"col-sm-4 col-sm-offset-2\"><div class=\"row\"><div class=\"form-group\" mw-container=\"form.uType\"><label for=\"uType\" class=\"col-sm-2 control-label\">Quoi*:</label><div class=\"col-sm-8\"><input type=\"text\" name=\"uType\" ng-model=\"vm.Model.Type\" class=\"form-control\" uib-typeahead=\"type for type in vm.Types | filter:$viewValue | limitTo:8\" mw-validate=\"vm.Rules.Type\"> <span mw-error=\"form.uType\"></span></div></div></div></div><div class=\"col-sm-4\"><div class=\"row\"><div class=\"form-group\" mw-container=\"form.uOu\"><label class=\"col-sm-2 control-label\">Ou* :</label><div class=\"col-sm-8\"><input type=\"text\" name=\"uOu\" ng-model=\"vm.Model.Ou\" class=\"form-control\" mw-validate=\"vm.Rules.Ou\"> <span mw-error=\"form.uOu\"></span></div></div></div></div><div class=\"col-sm-2\"><button type=\"submit\" class=\"btn btn-success\">Rechercher</button></div><div class=\"col-sm-12\"><a href=\"#\" ng-if=\"!vm.plus\" ng-click=\"vm.plus=true\">Plus de critères</a> <a href=\"#\" ng-if=\"vm.plus\" ng-click=\"vm.plus=false\">Moins de critères</a></div></form></div><div class=\"clearfix\"></div><div ng-if=\"vm.plus\"><h2>Dates</h2><div class=\"col-sm-4 col-sm-offset-2\"><div class=\"row\"><div class=\"form-group\" mw-container=\"form.uType\"><label for=\"uType\" class=\"col-sm-2 control-label\">Début:</label><div class=\"col-sm-8\"><input type=\"text\" name=\"uType\" ng-model=\"vm.Model.Type\" class=\"form-control\" uib-typeahead=\"type for type in vm.Types | filter:$viewValue | limitTo:8\" mw-validate=\"vm.Rules.Type\"> <span mw-error=\"form.uType\"></span></div></div></div></div><div class=\"col-sm-4\"><div class=\"row\"><div class=\"form-group\" mw-container=\"form.uOu\"><label class=\"col-sm-2 control-label\">Fin :</label><div class=\"col-sm-8\"><input type=\"text\" name=\"uOu\" ng-model=\"vm.Model.Ou\" class=\"form-control\" mw-validate=\"vm.Rules.Ou\"> <span mw-error=\"form.uOu\"></span></div></div></div></div><div class=\"clearfix\"></div><div class=\"clearfix\"></div><h2>Prix maximum</h2><div class=\"col-sm-8 col-sm-offset-2\"><input type=\"range\" min=\"1\" max=\"30\" step=\"1\" ng-model=\"vm.Model.Price\"><p class=\"text-center\" style=\"font-size:2em\" ng-bind=\"vm.Model.Price + ' €'\"></p></div><div class=\"clearfix\"></div><div class=\"form-group\"><div class=\"col-sm-3\"></div><div class=\"col-sm-4\"><button type=\"submit\" class=\"btn btn-success\">Rechercher</button></div></div></div><div ng-include=\"'breadcrumbAddBien.html'\"></div></div>"
+    "    </ol></script><div style=\"row\"><div ng-include=\"'breadcrumbAddBien.html'\"></div><h1>Rechercher</h1><div><form name=\"form\" role=\"form\" class=\"form-horizontal\" enctype=\"multipart/form-data\" novalidate mw-submit=\"vm.submit(form)\"><div class=\"col-sm-4 col-sm-offset-2\"><div class=\"row\"><div class=\"form-group\" mw-container=\"form.uType\"><label for=\"uType\" class=\"col-sm-2 control-label\">Quoi*:</label><div class=\"col-sm-8\"><input type=\"text\" name=\"uType\" ng-model=\"vm.Model.Type\" class=\"form-control\" uib-typeahead=\"type for type in vm.Types | filter:$viewValue | limitTo:8\" mw-validate=\"vm.Rules.Type\"> <span mw-error=\"form.uType\"></span></div></div></div></div><div class=\"col-sm-4\"><div class=\"row\"><div class=\"form-group\" mw-container=\"form.uOu\"><label class=\"col-sm-2 control-label\">Ou* :</label><div class=\"col-sm-8\"><input type=\"text\" name=\"uOu\" ng-model=\"vm.Model.Ou\" class=\"form-control\" mw-validate=\"vm.Rules.Ou\"> <span mw-error=\"form.uOu\"></span></div></div></div></div><div class=\"col-sm-2\"><button type=\"submit\" class=\"btn btn-success\">Rechercher</button></div><div class=\"col-sm-12\"><a href=\"#\" ng-if=\"!vm.plus\" ng-click=\"vm.plus=true\">Plus de critères</a> <a href=\"#\" ng-if=\"vm.plus\" ng-click=\"vm.plus=false\">Moins de critères</a></div></form></div><div class=\"clearfix\"></div><div ng-if=\"vm.plus\"><h2>Dates</h2><div class=\"col-sm-4 col-sm-offset-2\"><div class=\"row\"><div class=\"form-group\" mw-container=\"form.uType\"><label for=\"uType\" class=\"col-sm-2 control-label\">Début:</label><div class=\"col-sm-8\"><input type=\"text\" name=\"uType\" ng-model=\"vm.Model.Type\" class=\"form-control\" uib-typeahead=\"type for type in vm.Types | filter:$viewValue | limitTo:8\" mw-validate=\"vm.Rules.Type\"> <span mw-error=\"form.uType\"></span></div></div></div></div><div class=\"col-sm-4\"><div class=\"row\"><div class=\"form-group\" mw-container=\"form.uOu\"><label class=\"col-sm-2 control-label\">Fin :</label><div class=\"col-sm-8\"><input type=\"text\" name=\"uOu\" ng-model=\"vm.Model.Ou\" class=\"form-control\" mw-validate=\"vm.Rules.Ou\"> <span mw-error=\"form.uOu\"></span></div></div></div></div><div class=\"clearfix\"></div><div class=\"clearfix\"></div><h2>Prix maximum</h2><div class=\"col-sm-8 col-sm-offset-2\"><input type=\"range\" min=\"1\" max=\"30\" step=\"1\" ng-model=\"vm.Model.Price\"><p class=\"text-center\" style=\"font-size:2em\" ng-bind=\"vm.Model.Price + ' €'\"></p></div><div class=\"clearfix\"></div><div class=\"form-group\" style=\"height:60px\"><div class=\"col-sm-3\"></div><div class=\"col-sm-4\"><button type=\"submit\" class=\"btn btn-success\">Rechercher</button></div></div></div><div ng-include=\"'breadcrumbAddBien.html'\"></div></div>"
   );
 
 
